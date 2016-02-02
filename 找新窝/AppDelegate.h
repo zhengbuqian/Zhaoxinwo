67df5ade2754f840bfecee7d66fe8ad4ca1bc893
zhengbuqian <zhengbuqian@hotmail.com>
2016-02-02 13:51:55 +0800

    Initial Commit

---
+++ 找新窝/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  找新窝
+//
+//  Created by 卜千 郑 on 16/2/2.
+//  Copyright © 2016年 卜千 郑. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 找新窝/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  找新窝
+//
+//  Created by 卜千 郑 on 16/2/2.
+//  Copyright © 2016年 卜千 郑. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
